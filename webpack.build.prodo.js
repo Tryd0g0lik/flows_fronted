a3dbd039388d8f02e8ad8f0bc89987dfbe66485a
Denis <work80@mail.ru>
2025-10-01 13:11:39 +0700

    [master]: first commit

---
+++ webpack.build.prodo.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
+const merge = require('merge');
+const webpackConfig = require('./webpack.config.init.js');
+
+module.exports = merge(webpackConfig, {
+    mode: 'production',
+    cache: false,
+    output: {
+        path: path.resolve(__dirname, '../backend/static'),
+        filename: 'scripts/main-[id]-[fullhash].js',
+        publicPath: '/',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|jsx|ts|js)$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            configFile: path.resolve(__dirname, './babel.config.js'),
+                        },
+                    },
+                ],
+                exclude: [
+                    path.resolve(__dirname, '**/dist'),
+                    path.resolve(__dirname, 'node_modules'),
+                    path.resolve(__dirname, 'dist'),
+                ],
+            },
+
+            {
+                test: /\.s?[ac]ss$/i,
+                exclude: /\.module\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1 },
+                    },
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+
+    plugins: [
+        new Dotenv(),
+        new BundleTracker({
+            path: path.join(__dirname, '../bundles'),
+            filename: 'webpack-stats.json',
+        }),
+        new webpack.SourceMapDevToolPlugin({
+            test: /\.tsx?$/,
+            filename: '[file].map.[query]',
+            include: path.resolve(__dirname, '../bundles'),
+            columns: true,
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css',
+        }),
+    ],
+});
---
+++ webpack.config.init.js
@@ -0,0 +1,66 @@
+const path = require('path');
+
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+module.exports = {
+    mode: null,
+    cache: false,
+    entry: {
+        index: {
+            import: 'src\\index.ts',
+            dependOn: 'shared',
+        },
+        // https://webpack.js.org/guides/code-splitting/#entry-dependencies
+        another: {
+            import: './src/map/another-module.ts',
+            dependOn: 'shared',
+        },
+        shared: 'lodash',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    // https://webpack.js.org/guides/code-splitting/#entry-dependencies
+    optimization: {
+        runtimeChunk: 'single',
+        // minimize: false,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false, // out all comments
+                    },
+                },
+                extractComments: false, // dont save comments in separate files
+            }),
+        ],
+    },
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new ESLintPlugin({
+            files: path.resolve(__dirname, 'src/'),
+        }),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.jsx', '.ts', '.js', '.svg'],
+        plugins: [new TsconfigPathsPlugin()],
+        modules: [path.resolve(__dirname, 'node_modules')],
+        alias: {
+            '@interfeces': path.resolve(__dirname, 'src/interfaces.ts'),
+            // reduxToolkit: path.resolve(__dirname, 'src/reduxs'),
+            // pictures: path.resolve(__dirname, 'src/pictures'),
+            '@pages': path.resolve(__dirname, 'src/app'),
+        },
+    },
+};
